2457178610b9ded869e09ead2da6c188ed1d03b5
ArhamX24 <arhamabdullah9045@gmail.com>
2025-08-25 17:02:33 +0530

    updated

--- index.js
+++ index.js
@@ -15,12 +15,26 @@ const server = express()
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 server.use(cookieParser())
+
+const allowedOrigins = [
+  "http://www.pinkeyfootwear.in",
+  "https://pinkey-demo.netlify.app",
+  "http://localhost:5173",
+  "http://10.214.28.84:5173/"
+];
+
 server.use(cors({
-  // origin: "http://www.pinkeyfootwear.in",
-  origin: 'https://pinkey-demo.netlify.app',
-    // origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    // Allow requests with no origin like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Origin ${origin} not allowed by CORS`));
+    }
+  },
+  credentials: true
+}));
 
 server.use("/api/v1/auth", AuthRouter)
 server.use("/api/v1/admin", adminRouter)
